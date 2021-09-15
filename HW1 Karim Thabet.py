809393094ab8b36cf4e42292821f54ce594e3f20
karimthabet <90484071+karimthabet@users.noreply.github.com>
2021-09-15 17:24:00 -0400

    Add files via upload

---
+++ HW1 Karim Thabet.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Favoritecolors = ["blue","black,", "orange"]
+print(Favoritecolors)
+
+
+# In[2]:
+
+
+nums = list(range(30, 63, 3))
+print(nums)
+numsT = tuple(nums)
+print(numsT)
+
+
+# In[3]:
+
+
+list = []
+print(list)
+
+
+# In[4]:
+
+
+list.append(0)
+list.append(1)
+list.append(2)
+list.append(3)
+list.append(4)
+list.append(5)
+list
+
+
+# In[5]:
+
+
+del list[2]
+print(list)
+
+
+# In[6]:
+
+
+map(float,list)
+
+
+# In[7]:
+
+
+list.insert(2,2.0)
+print(list)
+
+
+# In[8]:
+
+
+len(list)
+
+
+# In[9]:
+
+
+max(list)
+
+
+# In[10]:
+
+
+min(list)
+
+
+# In[3]:
+
+
+newlist = list(range(1,11,1))
+print(newlist)
+
+
+# In[4]:
+
+
+sum(newlist)
+
+
+# In[ ]:
+
+
+
+
